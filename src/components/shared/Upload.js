39bd32ad74dd10ff9f2c93b386f940cbc977c3b8
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2022-12-12 11:31:09 -0600

    commented out the upload component

--- src/components/shared/Upload.js
+++ src/components/shared/Upload.js
@@ -1,72 +1,72 @@
-import React, { useState } from "react";
-import { Button, Form } from "react-bootstrap";
-import { imageCreate } from "../../api/image";
-import { useNavigate, useParams } from 'react-router-dom'
+// import React, { useState } from "react";
+// import { Button, Form } from "react-bootstrap";
+// import { imageCreate } from "../../api/image";
+// import { useNavigate, useParams } from 'react-router-dom'
 
-const Upload = ({ user, wishList }) => {
+// const Upload = ({ user, wishList }) => {
 
-	const [fileInputState, setFileInputState] = useState('')
+// 	const [fileInputState, setFileInputState] = useState('')
 
-	const [previewSource, setPreviewSource] = useState('')
+// 	const [previewSource, setPreviewSource] = useState('')
 
-	const { wishListId } = useParams()
-	const navigate = useNavigate()
+// 	const { wishListId } = useParams()
+// 	const navigate = useNavigate()
 
-	//show the user the picture they selected
-	const previewFile = (file) => {
-		//File reader is a built in js
-		const reader = new FileReader()
-		//converts image to string
-		reader.readAsDataURL(file)
-		reader.onloadend = () => {
-			setPreviewSource(reader.result)
-		}
-	}
+// 	//show the user the picture they selected
+// 	const previewFile = (file) => {
+// 		//File reader is a built in js
+// 		const reader = new FileReader()
+// 		//converts image to string
+// 		reader.readAsDataURL(file)
+// 		reader.onloadend = () => {
+// 			setPreviewSource(reader.result)
+// 		}
+// 	}
 
-	const handleFileInputChange = (e) => {
-		const file = e.target.files[0]
-		previewFile(file)
-	}
+// 	const handleFileInputChange = (e) => {
+// 		const file = e.target.files[0]
+// 		previewFile(file)
+// 	}
 
-	const uploadImage = async (previewSource) => {
-		let imgFile = previewSource
+// 	const uploadImage = async (previewSource) => {
+// 		let imgFile = previewSource
 
-		imageCreate(wishListId, user, imgFile)
-			.then(res => { navigate(`/santasHelper/${wishListId}`) })
-			.catch((error) => {
-				console.log(error)
-			})
-	}
+// 		imageCreate(wishListId, user, imgFile)
+// 			.then(res => { navigate(`/santasHelper/${wishListId}`) })
+// 			.catch((error) => {
+// 				console.log(error)
+// 			})
+// 	}
 
-	const handSubmitFile = (e) => {
-		e.preventDefault()
-		if (!previewSource) return;
-		uploadImage(previewSource)
-	}
+// 	const handSubmitFile = (e) => {
+// 		e.preventDefault()
+// 		if (!previewSource) return;
+// 		uploadImage(previewSource)
+// 	}
 
-	return (
-		<div>
-			<Form onSubmit={handSubmitFile} className='form'>
-				<Form.Group controlId="formFile" className="mb-3">
-					<Form.Label>Upload Your Image</Form.Label>
-					<Form.Control
-						type="file"
-						placeholder="Choose a picture"
-						onChange={handleFileInputChange}
-						name='image'
-						value={fileInputState}
-					/>
-					<small>picture must be a jpeg</small><br />
-					<Button variant="primary" type='submit'> Submit </Button>
-				</Form.Group>
+// 	return (
+// 		<div>
+// 			<Form onSubmit={handSubmitFile} className='form'>
+// 				<Form.Group controlId="formFile" className="mb-3">
+// 					<Form.Label>Upload Your Image</Form.Label>
+// 					<Form.Control
+// 						type="file"
+// 						placeholder="Choose a picture"
+// 						onChange={handleFileInputChange}
+// 						name='image'
+// 						value={fileInputState}
+// 					/>
+// 					<small>picture must be a jpeg</small><br />
+// 					<Button variant="primary" type='submit'> Submit </Button>
+// 				</Form.Group>
 
-			</Form>
-			{previewSource && (
-				<img src={previewSource} alt='chosen picture'
-					style={{ height: '300px' }} />
-			)}
-		</div>
-	)
-}
+// 			</Form>
+// 			{previewSource && (
+// 				<img src={previewSource} alt='chosen picture'
+// 					style={{ height: '300px' }} />
+// 			)}
+// 		</div>
+// 	)
+// }
 
-export default Upload
+// export default Upload
