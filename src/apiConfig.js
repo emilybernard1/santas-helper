c3ed51703d41269ede3d0699ab4fee7577d3b7ed
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2023-11-13 12:25:12 -0600

    trying to fix auth in production

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
 	// YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: 'https://santashelperbackend.fly.dev',
+	// production: 'https://santashelperbackend.fly.dev',
 	development: 'http://localhost:8080',
 }
 
